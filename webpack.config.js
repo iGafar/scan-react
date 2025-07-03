0184721354e78a9b1f2e47d3c7741fcd9e901b96
Gafar <gafar.isaev.2000@gmail.com>
2025-07-03 21:44:39 +0300

    fix: ci/cd

--- webpack.config.js
+++ webpack.config.js
@@ -5,76 +5,79 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: { scan: path.resolve(__dirname, './src/main.tsx') },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/',
-    clean: true,
-  },
-  module: {
-    rules: [
-      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const publicPath = isProduction ? '/scan-react/' : '/';
+
+  return {
+    mode: argv.mode || 'development',
+    entry: { scan: path.resolve(__dirname, './src/main.tsx') },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, './dist'),
+      publicPath: publicPath,
+      clean: true,
     },
-    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.css'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './index.html'),
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Scan',
-      template: './index.html',
-    }),
-    new MiniCssExtractPlugin({ filename: 'style.css' }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: 'public', to: '' }],
-    }),
-  ],
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin({
-        parallel: true,
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-          format: {
-            comments: false,
-          },
+    module: {
+      rules: [
+        { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
         },
-        extractComments: false,
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/',
+                publicPath: isProduction ? '/scan-react/images/' : '/images/',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+      },
+      extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.css'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './index.html'),
+        title: 'Scan',
+      }),
+      new MiniCssExtractPlugin({ filename: 'style.css' }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'public', to: '' }],
       }),
     ],
-  },
-  devServer: {
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-  },
+    optimization: {
+      minimizer: [
+        new CssMinimizerPlugin(),
+        new TerserPlugin({
+          parallel: true,
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+            format: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        }),
+      ],
+    },
+    devServer: {
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
 };
